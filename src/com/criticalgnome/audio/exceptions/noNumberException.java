d6eff2916d67a72bee3be2ca169dd399722ee67f
Сергей Калиновский <lord.skiminok@gmail.com>
2016-12-11 15:36:16 +0300

    Добавлена обработка исключений

--- src/com/criticalgnome/audio/exceptions/noNumberException.java
+++
@@ -1,8 +0,0 @@
-package com.criticalgnome.audio.exceptions;
-
-public class noNumberException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public noNumberException(Throwable e) { 
-        initCause(e); 
-    } 
-}
--- src/com/criticalgnome/audio/utils/Keyboard.java
+++ src/com/criticalgnome/audio/utils/Keyboard.java
@@ -1,5 +1,6 @@
 package com.criticalgnome.audio.utils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Keyboard {
@@ -8,7 +9,12 @@ public class Keyboard {
 	public static int inputNumber() {
 		int number = -1;
 		input = new Scanner(System.in);
-		number = input.nextInt();
-		return number;
+		try {
+			number = input.nextInt();
+			return number;
+		} catch (InputMismatchException e) {
+			return -1;
+		}
+		
 	}
 }
