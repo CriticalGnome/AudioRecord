9bf1a9c78f1d4371354cf79a91da5227fae4ae30
Сергей Калиновский <lord.skiminok@gmail.com>
2016-12-11 17:25:19 +0300

    Added saple data

--- src/com/criticalgnome/audio/AudioRecordRunner.java
+++ src/com/criticalgnome/audio/AudioRecordRunner.java
@@ -24,10 +24,39 @@ public class AudioRecordRunner {
 		
 		Collection collection = new Collection("My Collection", 0);
 		Track[] tracks = new Track[25];
-
+		Files[] files = new Track[25];
+		
 		// Тестовый блок
-		tracks[0] = new Track("Californication", 322, "Rock", "RHCP");
-		tracks[1] = new Track("Nothing else matters", 385, "Heavy Metal", "Metallica");
+
+		files[0] = new Track("TWIST", 215, "Rock-n-Roll", "Chubby Checker");
+		files[1] = new Track("SMOOTH", 253, "Latin Pop", "Santana Featuring Rob Thomas");
+		files[2] = new Track("MACK THE KNIFE", 192, "jazz", "Bobby Darin");
+		files[3] = new Track("HOW DO I LIVE", 268, "Pop", "LeAnn Rimes");
+		files[4] = new Track("PARTY ROCK ANTHEM", 263, "Electro House", "LMFAO Featuring Lauren Bennett & GoonRock");
+		files[5] = new Track("I GOTTA FEELING", 291, "Dance", "The Black Eyed Peas");
+		files[6] = new Track("MACARENA (BAYSIDE BOYS MIX)", 237, "Dance", "Los Del Rio");
+		files[7] = new Track("PHYSICAL", 223, "Dance", "Olivia Newton-John");
+		files[8] = new Track("YOU LIGHT UP MY LIFE", 209, "Pop", "Debby Boone");
+		files[9] = new Track("HEY JUDE", 428, "Pop-Rock", "The Beatles");
+		files[10] = new Track("WE BELONG TOGETHER", 200, "R&B", "Mariah Carey");
+		files[11] = new Track("UPTOWN FUNK!", 270, "Funk", "Mark Ronson Featuring Bruno Mars");
+		files[12] = new Track("UN-BREAK MY HEART", 272, "Pop", "Toni Braxton");
+		files[13] = new Track("YEAH!", 250, "Hip-Hop", "Usher Featuring Lil Jon & Ludacris");
+		files[14] = new Track("BETTE DAVIS EYES", 228, "Soft Rock", "Kim Carnes");
+		files[15] = new Track("ENDLESS LOVE", 268, "Pop", "Diana Ross & Lionel Richie");
+		files[16] = new Track("TONIGHT\'S THE NIGHT (GONNA BE ALRIGHT)", 236, "Soft Rock","Rod Stewart");
+		files[17] = new Track("FOOLISH GAMES / YOU WERE MEANT FOR ME", 253, "Folk", "Jewel");
+		files[18] = new Track("(EVERYTHING I DO) I DO IT FOR YOU", 394, "Soft Rock", "Bryan Adams");
+		files[19] = new Track("I'LL MAKE LOVE TO YOU", 244, "R&B", "Boyz II Men");
+		files[20] = new Track("THE THEME FROM \"A SUMMER PLACE\"", 145, "Easy Listening", "Percy Faith And His Orchestra");
+		files[21] = new Track("LE FREAK", 323, "Disco", "Chic");
+		files[22] = new Track("HOW DEEP IS YOUR LOVE", 246, "Pop", "Bee Gees");
+		files[23] = new Track("EYE OF THE TIGER", 246, "Rock", "Survivor");
+		files[24] = new Track("WE FOUND LOVE", 205, "Electro House", "Rihanna Featuring Calvin Harris");
+		
+		
+		
+		//tracks[0] = new Track("Californication", 322, "Rock", "RHCP");
 		// Конец тестового блока
 
 		while (true) {
