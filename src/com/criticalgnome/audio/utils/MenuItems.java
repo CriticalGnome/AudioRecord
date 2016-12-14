e157c6935578edeeb54f93409b9c1e32d2669a96
Сергей Калиновский <lord.skiminok@gmail.com>
2016-12-14 09:56:33 +0300

    Make shine

--- src/com/criticalgnome/audio/utils/MenuItems.java
+++ src/com/criticalgnome/audio/utils/MenuItems.java
@@ -10,29 +10,11 @@ import java.awt.Toolkit;
 import com.criticalgnome.audio.beans.Collection;
 import com.criticalgnome.audio.beans.Track;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class MenuItems.
  */
 public class MenuItems {
 	
-	/**
-	 * Main menu.
-	 */
-	public static void mainMenu() {
-		System.out.println(Variables.THICK_LINE);
-		System.out.println("\tAudio Recorder");
-		System.out.println(Variables.THIN_LINE);
-		System.out.println("1. Добавить трек в коллекцию");
-		System.out.println("2. Удалить трек из коллекции");
-		System.out.println("3. Отсортировать коллекцию");
-		System.out.println("4. Вывести список треков с заданной длительностью");
-		System.out.println("5. Записать коллекцию на диск");
-		System.out.println("0. Выход");
-		System.out.println(Variables.THIN_LINE);
-		System.out.print("Введите команду: ");
-
-	}
 	/**
 	 * Main menu.
 	 */
