65166f8857025d606926adf20eb9c35977d7e02d
Сергей Калиновский <lord.skiminok@gmail.com>
2016-12-14 10:01:48 +0300

    Make shine

--- src/com/criticalgnome/audio/utils/Variables.java
+++ src/com/criticalgnome/audio/utils/Variables.java
@@ -12,20 +12,22 @@ import com.criticalgnome.audio.beans.Track;
  */
 public class Variables {
 
+	/** Thick line decoration string */
+	public static final String THICK_LINE = "\n==================================================================";
+	/** Thin line decoration  string */
+	public static final String THIN_LINE = "------------------------------------------------------------------";
 	/** Error string */
 	public static final String INVALID_INPUT = "Неверный ввод. Попробуйте ещё раз.";
 	/** Under construction string */
 	public static final String NOT_REALISED = "Пока не реализовано.";
 	/** Default duration of CD-Disk (in seconds) */
-	public static final String THIN_LINE = "------------------------------------------------------------------";
-	public static final String THICK_LINE = "\n==================================================================";
 	public static final int DISK_DURATION = 4800; //80 min
-	public static final int EXIT = 0;
-	public static final int ADD_TRACK = 1;
-	public static final int DEL_TRACK = 2;
-	public static final int SORT_COLLECTION = 3;
-	public static final int TAKE_BY_DURATION = 4;
-	public static final int RECORD_DISK = 5;
+	public static final int EXIT = 0; // Key 0 from keyboard
+	public static final int ADD_TRACK = 1; // Key 1 from keyboard
+	public static final int DEL_TRACK = 2; // Key 2 from keyboard
+	public static final int SORT_COLLECTION = 3; // Key 3 from keyboard
+	public static final int TAKE_BY_DURATION = 4; // Key 4 from keyboard
+	public static final int RECORD_DISK = 5; // Key 5 from keyboard
 	
 	/**
 	 * Fill files list by fabricated evidence.
