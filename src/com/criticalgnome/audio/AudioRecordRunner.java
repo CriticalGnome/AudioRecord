be9023d4529f000adb887f869e2c7c58f80184fd
lords <lords@Skiminok>
2016-12-26 17:10:10 +0300

    Добавил список заданий

--- src/com/criticalgnome/audio/AudioRecordRunner.java
+++ src/com/criticalgnome/audio/AudioRecordRunner.java
@@ -5,8 +5,17 @@
  */
 package com.criticalgnome.audio;
 
+// TODO Реализовать в итоговом проекте счетчик объектов через static
+// TODO Реализовать в итоговом проекте модуля custom exceptions и их корректную обработку
+// TODO Реализовать +List+, Set, Map в итоговом проекте
+// TODO Реализовать запись/чтение символьной и байтовой информации в файл в итоговом проекте
+// TODO Реализовать сериализацию в итоговом проекте (иерархия, static, transient)
+// TODO Использовать ListIterator в итоговом проекте
+
 import java.awt.Toolkit;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.criticalgnome.audio.beans.*;
@@ -21,6 +30,14 @@ public class AudioRecordRunner {
 	 *            Command line arguments
 	 */
 	public static void main(String[] args) {
+		
+		Calendar calendar = new GregorianCalendar();
+		System.out.print("Добро пожаловать. Сегодня "
+		+ calendar.get(Calendar.DATE) + "." 
+		+ (calendar.get(Calendar.MONTH) + 1) + "." 
+		+ calendar.get(Calendar.YEAR) + ", местное время "
+		+ calendar.get(Calendar.HOUR_OF_DAY) + ":"
+		+ calendar.get(Calendar.MINUTE));
 
 		List<Track> tracks = new ArrayList<Track>();
 		
